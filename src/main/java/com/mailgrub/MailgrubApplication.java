e2523cb7a85a5b2a9044d3d95de3b5d4d9ef1f1c
Arthur Revelski <arthurrevelski@Arthurs-Mac-mini.local>
2025-09-21 18:20:32 -0600

    added caffiene for basic cacheing

--- src/main/java/com/mailgrub/MailgrubApplication.java
+++ src/main/java/com/mailgrub/MailgrubApplication.java
@@ -1,9 +1,11 @@
 package com.mailgrub;
 
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableCaching
 public class MailgrubApplication {
 
   public static void main(String[] args) {
